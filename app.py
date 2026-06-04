9cba6b4a29368036479f3c0e2271b83b90e9b77c
Chiragjsca <chiragjsca@gmail.com>
2026-06-04 07:45:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ st.markdown(hide_streamlit_ui, unsafe_allow_html=True)
 # ==========================================
 # 🔐 ADMIN LOGIN SYSTEM
 # ==========================================
-ADMIN_PASSWORD = "dada"
+ADMIN_PASSWORD = "dada1"
 
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
