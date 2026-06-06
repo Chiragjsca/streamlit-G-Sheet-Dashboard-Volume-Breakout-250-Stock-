142cfd45ef0a91dad4e72f3fb0660f0aae5f9ad1
Chiragjsca <chiragjsca@gmail.com>
2026-06-06 05:56:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ from datetime import datetime
 # ==========================================
 # 🔐 ADMIN LOGIN SYSTEM
 # ==========================================
-ADMIN_PASSWORD = "dada krishna"
+ADMIN_PASSWORD = "dadakrishna"
 
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
